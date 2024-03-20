a3c612d1e53bed801118f35e890c24eed1527977
Carlos Felice <carlosfelicelxn@gmail.com>
2024-03-20 17:35:40 -0300

    cards mejoradas

--- front/scripts/renderCards.js
+++ front/scripts/renderCards.js
@@ -9,7 +9,7 @@ function renderCards(data) {
   data.forEach((movie) => {
     // div card
     const card = document.createElement("div");
-    card.classList.add("card");
+    card.classList.add("card"); // Agrega la clase "card"
 
     // div card-body
     const cardBody = document.createElement("div");
@@ -28,13 +28,40 @@ function renderCards(data) {
     // card-text
     const year = document.createElement("p");
     year.classList.add("card-text");
-    year.innerHTML = movie.year;
+    year.innerHTML = `<strong>Año:</strong> ${movie.year}`;
+
+    const director = document.createElement("p");
+    director.classList.add("card-text");
+    director.innerHTML = `<strong>Director:</strong> ${movie.director}`;
+
+    const duration = document.createElement("p");
+    duration.classList.add("card-text");
+    duration.innerHTML = `<strong>Duración:</strong> ${movie.duration}`;
+
+    const genre = document.createElement("p");
+    genre.classList.add("card-text");
+    genre.innerHTML = `<strong>Género:</strong> ${movie.genre}`;
+
+    const rate = document.createElement("p");
+    rate.classList.add("card-text");
+    rate.innerHTML = `<strong>Rate:</strong> ${movie.rate}`;
+
+    const description = document.createElement("p");
+    description.classList.add("card-text");
+    description.innerHTML = `<strong>Descripción:</strong> ${movie.description}`;
 
     card.appendChild(poster);
     card.appendChild(title);
     card.appendChild(year);
+    card.appendChild(director);
+    card.appendChild(duration);
+    card.appendChild(genre);
+    card.appendChild(rate);
+    card.appendChild(description);
+
     moviesContainer.appendChild(card);
   });
 }
 
 module.exports = renderCards;
+
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -6,4 +6,6 @@ module.exports = {
     path: __dirname + "/public",
     filename: "bundle.js",
   },
+
+  
 };
